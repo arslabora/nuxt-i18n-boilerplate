59cef028d5f9754aefd4fcb7cf085d6a0ef84541
spadino <spada.andrea@gmail.com>
2021-04-18 22:24:13 -0300

    Initiate a boilerplate with i18n enabled

---
+++ nuxt.config.js
@@ -0,0 +1,71 @@
+export default {
+  // Target: https://go.nuxtjs.dev/config-target
+  target: 'static',
+
+  // Global page headers: https://go.nuxtjs.dev/config-head
+  head: {
+    title: 'liuteriadautore',
+    meta: [
+      {charset: 'utf-8'},
+      {name: 'viewport', content: 'width=device-width, initial-scale=1'},
+      {hid: 'description', name: 'description', content: ''},
+    ],
+    link: [{rel: 'icon', type: 'image/x-icon', href: '/favicon.ico'}],
+  },
+
+  // Global CSS: https://go.nuxtjs.dev/config-css
+  css: [],
+
+  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
+  plugins: [],
+
+  // Auto import components: https://go.nuxtjs.dev/config-components
+  components: true,
+
+  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
+  buildModules: [
+    // https://go.nuxtjs.dev/eslint
+    '@nuxtjs/eslint-module',
+    'nuxt-windicss',
+  ],
+
+  // Modules: https://go.nuxtjs.dev/config-modules
+  modules: [
+    // https://go.nuxtjs.dev/pwa
+    '@nuxtjs/pwa',
+    // https://go.nuxtjs.dev/content
+    '@nuxt/content',
+    'nuxt-i18n',
+  ],
+
+  i18n: {
+    locales: [
+      {code: 'en', iso: 'en-US', file: 'en.js'},
+      {code: 'it', iso: 'it-IT', file: 'it.js'},
+      {code: 'pt', iso: 'pt-BR', file: 'pt.js'},
+      {code: 'fr', iso: 'fr-FR', file: 'fr.js'},
+      {code: 'es', iso: 'es-ES', file: 'es.js'},
+    ],
+    vueI18nLoader: true,
+    defaultLocale: 'en',
+    strategy: 'prefix',
+    seo: 'true',
+    langDir: '~/locales/',
+    vueI18n: {
+      fallbackLocale: 'en',
+    },
+  },
+
+  // PWA module configuration: https://go.nuxtjs.dev/pwa
+  pwa: {
+    manifest: {
+      lang: 'en',
+    },
+  },
+
+  // Content module configuration: https://go.nuxtjs.dev/config-content
+  content: {},
+
+  // Build Configuration: https://go.nuxtjs.dev/config-build
+  build: {},
+};
---
+++ pages/index.vue
@@ -0,0 +1,84 @@
+<i18n>
+{
+  "en": {
+    "hello": "hello world!"
+  },
+  "it": {
+    "hello": "Ciao mondo!"
+  },
+  "pt": {
+    "hello": "Olá mundo!"
+  },
+  "fr": {
+    "hello": "Bonjour monde!"
+  },
+  "es": {
+    "hello": "Hola mondo!"
+  }
+}
+</i18n>
+
+<template>
+  <div class="container">
+    <div>
+      <h2 class="mt-10">{{ $t('hello') }}</h2>      
+      <Logo />
+      <h1 class="title">liuteriadautore</h1>
+      <div class="links">
+        <a
+          href="https://nuxtjs.org/"
+          target="_blank"
+          rel="noopener noreferrer"
+          class="button--green"
+        >
+          Documentation
+        </a>
+        <a
+          href="https://github.com/nuxt/nuxt.js"
+          target="_blank"
+          rel="noopener noreferrer"
+          class="button--grey"
+        >
+          GitHub
+        </a>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+.container {
+  margin: 0 auto;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+
+.title {
+  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
+    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
+  display: block;
+  font-weight: 300;
+  font-size: 100px;
+  color: #35495e;
+  letter-spacing: 1px;
+}
+
+.subtitle {
+  font-weight: 300;
+  font-size: 42px;
+  color: #526488;
+  word-spacing: 5px;
+  padding-bottom: 15px;
+}
+
+.links {
+  padding-top: 15px;
+}
+</style>
